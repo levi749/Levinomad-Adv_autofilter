9c0ba062b799088dce3e5196d6c1bedfe712394e
levi749 <84772173+levi749@users.noreply.github.com>
2021-06-05 09:34:32 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -95,7 +95,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⚜️ Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} ⚜️", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/Levinomad_TG'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://t.me/CLALL1')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Support 🛠', url='https://t.me/cl_chatgrp')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
